083928492284ab46d43b9fccc8d4d49e1501f889
Khuirul-Huda <hudah2102@gmail.com>
2022-06-29 23:48:17 +0700

    database functionality

--- ayang.php
+++ ayang.php
@@ -1,22 +1,23 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 
-    $db = new SQLite3('./database-test.db');
+    $db = new SQLite3('./database-main.db');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-    $content = $db->query('SELECT * FROM main');
+    $content = $db->query('SELECT * FROM main ORDER BY id DESC');
     $res = array();
     while ($tmp = $content->fetchArray(SQLITE3_ASSOC)) {
         $res[] = $tmp;
     }
      echo(json_encode($res));
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
 $data = file_get_contents('php://input');
 $data = json_decode($data, true);
 $nameVal = $data['name'];
 $msgVal = $data['msg'];
-$timeVal = date('d') . " " . date("F", mktime(0, 0, 0, date('m'), 10)) . " " . date('Y') . " " . date('H:i');
+$timeVal = base64_encode(date('d') . " " . date("F", mktime(0, 0, 0, date('m'), 10)) . " " . date('Y') . " " . date('H:i'));
 //TODO: USE PREPARE()
 $dbInput = $db->exec("INSERT INTO main(name, msg, time) VALUES ('$nameVal', '$msgVal', '$timeVal')");
 
